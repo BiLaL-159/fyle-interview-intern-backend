4c30e6282248024c11727a64e405bb5ef3bd30dd
BILAL <bilalaliofficial1@gmail.com>
2024-07-19 10:34:40 +0530

    Modified the api endpoint to correct endpoint

--- core/apis/teachers/principal.py
+++ core/apis/teachers/principal.py
@@ -8,7 +8,7 @@ from core.libs import assertions
 
 principal_teachers_resources = Blueprint('principal_teachers_resources', __name__)
 
-@principal_teachers_resources.route('/principal/teachers', methods=['GET'], strict_slashes=False)
+@principal_teachers_resources.route('/teachers', methods=['GET'], strict_slashes=False)
 @decorators.authenticate_principal
 def list_teachers(p):
     """Returns list of teachers"""
