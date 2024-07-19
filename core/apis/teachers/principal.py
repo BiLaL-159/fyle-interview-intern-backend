fedf5573daf7e6ad991dd2ab8f09c4f433499cff
BILAL <bilalaliofficial1@gmail.com>
2024-07-19 09:31:01 +0530

    Added the code for the principal_teacher's api

---
+++ core/apis/teachers/principal.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+from core import db
+from core.apis import decorators
+from core.apis.responses import APIResponse
+from core.models.teachers import Teacher
+from .schema import TeacherSchema
+from core.libs import assertions
+
+principal_teachers_resources = Blueprint('principal_teachers_resources', __name__)
+
+@principal_teachers_resources.route('/principal/teachers', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def list_teachers(p):
+    """Returns list of teachers"""
+    
+    teachers = Teacher.query.all()
+    teacher_schema = TeacherSchema(many=True)
+    teachers_dump = teacher_schema.dump(teachers)
+    return APIResponse.respond(data=teachers_dump)
